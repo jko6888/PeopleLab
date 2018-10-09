9e02307d9f047477b6d67432effb83fab44bdc0c
jko6888 <43213100+jko6888@users.noreply.github.com>
2018-10-09 11:23:26 -0400

    Create Classroom.java

---
+++ src/JamesKo_SabrinaLi_EugeneK/Classroom.java
@@ -0,0 +1,15 @@
+package JamesKo_SabrinaLi_EugeneK;
+
+public class Classroom {
+    Student[] student;
+    Teacher teacher;
+
+    public Classroom(Student[] student, Teacher teacher) {
+        this.student = student;
+        this.teacher = teacher;
+    }
+
+    public String getSubject() {
+        return this.teacher.getSubject();
+    }
+}
