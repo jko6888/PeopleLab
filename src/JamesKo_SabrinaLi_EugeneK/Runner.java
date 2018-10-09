76f933f00621f3d88279b9d65a415dbf375b24bf
jko6888 <43213100+jko6888@users.noreply.github.com>
2018-10-09 11:10:32 -0400

    Update Runner.java

--- src/JamesKo_SabrinaLi_EugeneK/Runner.java
+++ src/JamesKo_SabrinaLi_EugeneK/Runner.java
@@ -1,8 +1,7 @@
 package JamesKo_SabrinaLi_EugeneK;
 
 public class Runner {
-    public static String[] firstNames = {"Bob","Ernesto","Henry","Ben","Jennifer","Amy","Caroline","Justin","Greg","Nathan","Ivan"};
-    public static String[] familyNames = {"Wolf","Edwards","Gibbs","White","Brown","Li","Hughes","Brady","Rodriguez","Turner","Owen"};
+
     public static void main(String[] args) {
 
     }
